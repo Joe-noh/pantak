9b5636a0eca383e3eb61a8dc6712a6d30f48a35f
Joe-noh <goflb.jh@gmail.com>
2015-06-26 12:24:51 +0900

    add user validation and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ActiveRecord::Base
+  validates :full_name,  presence: true
+  validates :nick_name,  presence: true
+  validates :job_type,   presence: true
+  validates :emp_number, presence: true, numericality: {greater_than: 0}
 end
