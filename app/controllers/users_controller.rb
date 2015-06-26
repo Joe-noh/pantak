b2695dc1e14a39fbfd3bf06992179aa6e3d04e9d
Joe-noh <goflb.jh@gmail.com>
2015-06-26 16:54:04 +0900

    signup users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,11 +12,26 @@ class UsersController < ApplicationController
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to "/"
+    else
+      render "new"
+    end
   end
 
   def update
   end
 
   def destroy
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :full_name, :nick_name, :job_type, :self_introduction,
+      :password, :password_confirmation
+    )
   end
 end
