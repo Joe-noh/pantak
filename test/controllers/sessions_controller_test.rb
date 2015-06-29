3691ebabd3e5ad35304f2d00d16cca49ba14f92c
Joe-noh <goflb.jh@gmail.com>
2015-06-29 11:36:17 +0900

    add some test helpers

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -5,10 +5,4 @@ class SessionsControllerTest < ActionController::TestCase
     get :new
     assert_response :success
   end
-
-  test "should get create" do
-    get :create
-    assert_response :success
-  end
-
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -6,5 +6,27 @@ class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
 
-  # Add more helper methods to be used by all tests here...
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
+
+  def log_in_as(user, options = {})
+    password = options[:password] || "password"
+
+    if integration_test?
+      session_params = {
+        email:       user.email,
+        password:    password
+      }
+      post login_path, session: session_params
+    else
+      session[:user_id] = user.id
+    end
+  end
+
+  private
+
+  def integration_test?
+    defined?(post_via_redirect)
+  end
 end
