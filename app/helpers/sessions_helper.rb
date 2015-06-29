37006e90a282766b6acff041342e392fe170e7cc
Joe-noh <goflb.jh@gmail.com>
2015-06-29 11:24:24 +0900

    hide topb-ar links when not logged in

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -3,6 +3,17 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  def current_user
+    user_id = session[:user_id]
+    if (user_id)
+      @current_user ||= User.find_by(id: user_id)
+    end
+  end
+
+  def logged_in?
+    !(current_user.nil?)
+  end
+
   def redirect_back_or(default)
     redirect_to(session[:forwarding_url]  || default)
     session.delete(:forwarding_url)
