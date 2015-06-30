908564d7002a305e1345b1571b92f3bc49e432f6
Joe-noh <goflb.jh@gmail.com>
2015-06-30 15:52:43 +0900

    validate comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,8 @@
 class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :diary
+
+  validates :user_id,  presence: true
+  validates :diary_id, presence: true
+  validates :content,  presence: true
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,26 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @comment = comments(:one)
+  end
+
+  test "should be valid" do
+    assert @comment.valid?
+  end
+
+  test "user_id should be present" do
+    @comment.user_id = nil
+    assert_not @comment.valid?
+  end
+
+  test "diary_id should be present" do
+    @comment.diary_id = nil
+    assert_not @comment.valid?
+  end
+
+  test "content should be present" do
+    @comment.content = nil
+    assert_not @comment.valid?
+  end
 end
